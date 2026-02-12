10da33a9598542681766308d6e2c3d33f8e7e3fc
Priyanshu Pandey <priyanshun14314@gmail.com>
2026-02-12 23:14:39 +0530

    Initial project structure commit

---
+++ src/main/java/com/learning/SB_JDBC/SbJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.learning.SB_JDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/SB_JDBC/SbJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.SB_JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
